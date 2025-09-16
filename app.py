a67eb2f36c3ea3cf9e46ba4566b773d334fb872a
Yash <yashshinde6380@gmail.com>
2025-09-16 18:37:55 +0530

    app and docker updated

--- app.py
+++ app.py
@@ -92,5 +92,8 @@ def predict_skymotion():
     }
     return jsonify(response)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
